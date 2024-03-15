81642065cbdc996ff51a47d5459deb1f52f56259
Måns <mans.holmgren@gmail.com>
2024-03-15 17:53:15 +0100

    Update mandelbrot_cy.pyx

--- mandelbrot_cy.pyx
+++ mandelbrot_cy.pyx
@@ -1,7 +1,6 @@
 # mandelbrot_cy.pyx
 
-cdef extern from "complex.h":
-    double cabs(double complex)
+from libc.math cimport abs as c_abs  # Use Cython's wrapped function for absolute value
 
 cdef inline double complex c_square(double complex c):
     return c * c
@@ -9,7 +8,7 @@ cdef inline double complex c_square(double complex c):
 cpdef int mandelbrot(double complex c, int max_iter):
     cdef double complex z = 0
     cdef int n = 0
-    while cabs(z) <= 2 and n < max_iter:
+    while c_abs(z) <= 2 and n < max_iter:  # Use Cython's c_abs for complex numbers
         z = c_square(z) + c
         n += 1
     return n
